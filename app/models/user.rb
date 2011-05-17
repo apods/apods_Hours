ad67e3c63beeae6a3f26866f5022fba9ac8ab0d3
Gino Sanzi <eugene.sanzi@gmail.com>
2011-05-16 20:35:53 -0700

    Finished user model, some other minor additions like a user route, adding the less gem to environment.rb, and some users added for testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110515222920
+# Schema version: 20110516015845
 #
 # Table name: users
 #
@@ -15,11 +15,17 @@
 #  updated_at         :datetime
 #
 
+require 'digest'
 class User < ActiveRecord::Base
   
-  attr_accessor :first_name, :last_name, :email
+  before_save :encrypt_password
   
-  validates_presence_of :first_name, :last_name, :email, :peoplesoft_num
+  attr_accessor :password
+  attr_accessible :first_name, :last_name, :peoplesoft_num, :email, :password, :password_confirmation
+  
+  EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates_presence_of :first_name, :last_name, :email, :peoplesoft_num, :password
   
   validates_length_of :first_name, :maximum => 32
   validates_length_of :last_name, :maximum => 32
@@ -33,4 +39,41 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :case_sensitive => false
   validates_uniqueness_of :peoplesoft_num
   
+  validates_confirmation_of :password
+  validates_length_of :password, :within => 6..40
+  
+  
+  #Authenticate a user, return nil if user does not exist or password doesn't match
+  def self.authenticate(submitted_peoplesoft_num, submitted_password)
+    user = find_by_peoplesoft_num(submitted_peoplesoft_num)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  #Check to see if submitted password is correct
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  #Private methods for encrypting passwords and creating encrypted passwords
+  private
+    
+    def encrypt_password
+      self.password_salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{password_salt}#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+    
+    
 end
