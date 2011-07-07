cd725ca322f28039538e264fee95ff9e0d44b70b
Gino Sanzi <eugene.sanzi@gmail.com>
2011-07-06 22:27:39 -0700

    Continued work on user sign in, all sign in functions should be working now.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,13 @@ class SessionsController < ApplicationController
   
   def create
     @user = User.authenticate(params[:session][:peoplesoft_num], params[:session][:password])
-    if @user.nil?
+    if @user.nil?   # authentication was unsuccessful, there is no matching record in the database for this user
       flash[:error] = "Invalid input, make sure you entered your\npeoplesoft number and password correctly.\nIf you have not registered this semester, please do so."
       redirect_to login_path
     end
+    
+    # user was authenticated, proceed to sign them in
+    sign_in @user
     # for the benefit of new people using this, flash.now is used
     # here because there is an implicit render at the end of every
     # function, regular flash is used before a redirect.
@@ -19,6 +22,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    sign_out
+    redirect_to root_path
   end
   
 end
