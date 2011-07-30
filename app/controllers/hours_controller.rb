3d813311af638625cf1d8bd6313c3a66b44e1828
Gino Sanzi <eugene.sanzi@gmail.com>
2011-07-30 14:40:40 -0700

    Addition of a new hours entry link and a file for the new hours form

--- app/controllers/hours_controller.rb
+++ app/controllers/hours_controller.rb
@@ -7,5 +7,17 @@ class HoursController < ApplicationController
   def index
     @hours = Hour.find_all_by_user_id(current_user.id)
   end
+  
+  def new
+    @hour = Hour.new
+  end
+  
+  def edit
+    
+  end
+  
+  def destroy
+    
+  end
 
 end
