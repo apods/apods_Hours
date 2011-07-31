240480da0ad44e0a33974f286c9d61f74ec768b5
Gino Sanzi <eugene.sanzi@gmail.com>
2011-07-30 20:52:38 -0700

    Added edit page and associated logic as well as some security to the update method. At the moment update does not work properly and doesn't save to the database.

--- app/controllers/hours_controller.rb
+++ app/controllers/hours_controller.rb
@@ -3,7 +3,7 @@ class HoursController < ApplicationController
   layout 'main_layout'
   
   before_filter :require_login
-  before_filter :authorized_user, :only => :destroy
+  before_filter :authorized_user, :only => [:update, :destroy]
   
   def index
     @hours = Hour.find_all_by_user_id(current_user.id)
@@ -16,7 +16,7 @@ class HoursController < ApplicationController
   def create
     @hour = current_user.hours.build(params[:hour])
     if @hour.save
-      flash[:success] = "Your hour entry has been saved to the database!"
+      flash[:success] = "Your hour entry has been saved!"
       redirect_to hours_path
     else
       flash[:error] = "Error, your entry was not saved, please try again. " + 
@@ -27,7 +27,19 @@ class HoursController < ApplicationController
   end
   
   def edit
-    
+    @hour = Hour.find(params[:id])
+  end
+  
+  def update
+    if @hour.save
+      flash[:success] = "Changes saved!"
+      redirect_to hours_path
+    else
+      flash[:error] = "Error, your changes were not saved, please try again. " + 
+                      "If you receive this message again, contact Gino Sanzi " + 
+                      "at eugene.sanzi@gmail.com"
+      redirect_to edit_hour_path
+    end
   end
   
   def destroy
